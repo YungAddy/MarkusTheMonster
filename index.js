61e8ba3cf8c3524372cfdc9f0c0c3e19a4624f5f
YungAddy <gdomino30@gmail.com>
2022-06-20 17:17:56 -0400

    This is a website I made for my friend. I Found it on a website and tweaked it!

---
+++ index.js
@@ -0,0 +1,7 @@
+import Typed from "https://cdn.jsdelivr.net/npm/typed.js@2.0.12"
+var options = {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 40
+};
+
+var typed = new Typed('#markusTyped', options);
